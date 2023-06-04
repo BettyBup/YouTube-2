f24d9ba7d3bc837fafe41e334a1dc15bb43f89e2
BettyBup <134177975+BettyBup@users.noreply.github.com>
2023-06-04 16:54:25 -0400

    script.js

--- script.js
+++ script.js
@@ -27,7 +27,80 @@ function plusSlides(n) {
   var slideText = slides[slideIndex-1].getElementsByTagName('img')[0].alt;
   document.getElementById('slide-text').innerHTML = "<h2>" + slideText + "</h2>";
 }
+function darkMode(){
 
+  let element = document.body;
+
+  element.classList.toggle("dark-mode");
+
+  element.classList.contains("dark-mode") ? localStorage.setItem("darkMode","enabled") : localStorage.setItem("darkMode",null)
+
+}
+function scrollToTop() {
+
+  let scrollY = window.scrollY;
+
+  let distance = 200;
+
+  let speed = 20;
+
+  let currentY = scrollY;
+
+  let targetY = 0;
+
+  let body = document.body;
+
+  let requestId;
+
+  function easeOutQuad(t, b, c, d) {
+
+    t /= d;
+
+    return -c * t*(t-2) + b;
+
+  };
+
+  function animate() {
+
+    var currentTime = Date.now();
+
+    var time = Math.min(1, ((currentTime - startTime) / duration));
+
+    var easing = easeOutQuad(time, 0, 1, 1);
+
+    window.scrollTo(0, (easing * (targetY - startY)) + startY);
+
+    if (time < 1) {
+
+      requestId = requestAnimationFrame(animate);
+
+    } else {
+
+      cancelAnimationFrame(requestId);
+
+    }
+
+  }
+
+  if (body.classList.contains('smooth-scroll')) {
+
+    var startY = currentY;
+
+    var startTime = Date.now();
+
+    var duration = Math.abs(targetY - startY) / speed;
+
+    requestId = requestAnimationFrame(animate);
+
+  }
+
+  else {
+
+    window.scrollTo(0, targetY);
+
+  }
+
+}
 
 
 
