d87218cc9839e205055dc498ca02b407d260ee13
BettyBup <134177975+BettyBup@users.noreply.github.com>
2023-06-04 16:31:27 -0400

    script.js

--- script.js
+++ script.js
@@ -1,47 +1,74 @@
-var swiper = new Swiper(".mySwiper-1",{
-    slidesPerView:1,
-    spaceBetween: 30,
-    loop:true,
-    pagination: {
-        el:".swiper-pagination",
-        clickable:true,
-    },
-    navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
-    }
-});
+var slideIndex = 0;
+showSlides();
 
-var swiper = new Swiper(".mySwiper-2",{
-    slidesPerView:3,
-    spaceBetween: 20,
-    loop:true,
-    loopFillGroupWithBlank:true
-    navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
-    },
-    breakpoints : {
-        0: {
-            slidesPerView:1,
-        },
-        520: {
-            slidesPerView:2,
-        },
-        950: {
-            slidesPerView:3,
-        }
-    }
-});
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  var slideText = slides[slideIndex-1].getElementsByTagName('img')[0].alt;
+  document.getElementById('slide-text').innerHTML = "<h2>" + slideText + "</h2>";
+  setTimeout(showSlides, 4000); // Cambia la imagen cada 4 segundos
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  var slides = document.getElementsByClassName("slide");
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  var slideText = slides[slideIndex-1].getElementsByTagName('img')[0].alt;
+  document.getElementById('slide-text').innerHTML = "<h2>" + slideText + "</h2>";
+}
 
 
-let tabInputs = document.querySelectorAll(".tabInput");
-tabInputs.forEach(function(input) {
 
-    input.addEventListener('change', function() {
-        let id = input.ariaValueMax;
-        let thisSwiper = document.getElementById('swiper' + id);
-        thisSwiper.swuiper.update();
-    })
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
